3745dce90341f713ac6d229cc5f4d9823ace72c4
Natalia Fedorova <natalafedorova268@gmail.com>
2022-09-28 21:17:50 +0300

    task27

--- дз 4/Program.cs
+++ дз 4/Program.cs
@@ -3,6 +3,7 @@
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
 void task25()
 {
 Console.WriteLine("Введите число A");
@@ -16,6 +17,27 @@ for (int i = 2; i <= secondnumber; i++)
 }
 Console.WriteLine($"{firstnumber} в степени {secondnumber} = {power}");
 }
+// task25();
 
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void task27()
+{
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int sum = 0;
+int num = number;
+while (number > 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+Console.WriteLine($"Симма цифр числа {num} равна {sum}");
+}
+
+task27();
 

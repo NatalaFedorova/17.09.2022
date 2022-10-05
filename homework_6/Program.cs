f62e1ea335778c3d6c327749dfb9f6ba15b74aed
Natalia Fedorova <natalafedorova268@gmail.com>
2022-10-05 21:06:43 +0300

    task 41 is done

---
+++ homework_6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+void Task41()
+{
+Console.WriteLine("Сколько чисел вы хотите проверить?");
+int allNumbers = Convert.ToInt32(Console.ReadLine());
+int countPositiveNumber = 0;
+for (int i= 0; i < allNumbers; i++)
+{
+    Console.WriteLine("Ведите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) countPositiveNumber++;
+}
+Console.WriteLine($"{countPositiveNumber} чисел больше нуля");
+}
+// Task41();

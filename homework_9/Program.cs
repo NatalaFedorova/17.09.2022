62fc24fcc999b3cc51a608e342d5d7cba279fea2
Natalia Fedorova <natalafedorova268@gmail.com>
2022-10-15 18:50:27 +0300

    Task 66, 68

--- homework_9/Program.cs
+++ homework_9/Program.cs
@@ -40,8 +40,61 @@ void FindNumbersDividingTree(int m, int n, int count = 0)
     FindNumbersDividingTree(m, n, count);
 }
 
+void Task66()
+{
+// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int m = InputNomber("Введите натуральное число M");
+int n = InputNomber("Введите натуральное число N");
+if (m > n)
+{
+    (m, n) = (n, m);
+}
+Console.Write($"Сумма натуральных чисел от {m} до {n} = ");
+SumNumbers(m, n);
+Console.WriteLine();
+}
+void SumNumbers(int m, int n, int sum = 0)
+{
+sum = sum + m;   
+if (m >= n)
+{
+    Console.Write(sum);
+    return;
+}
+m++;
+SumNumbers(m, n, sum);
+}
 
+void Task68()
+{
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+int n = InputNomber("Введите неотрицательное число M");
+int m = InputNomber("Введите неотрицательное число N");
+int a = AkkermanFunction (n, m);
+Console.WriteLine($"A({n}, {m}) = {a}");
+}
 
+int AkkermanFunction(int n, int m)
+{
+if (n == 0) 
+{
+    return m + 1;
+}
+else if ((n > 0) && (m == 0)) 
+{
+    return AkkermanFunction(n - 1, 1);
+}
+else 
+{
+    return AkkermanFunction(n - 1, AkkermanFunction(n, m - 1));
+}
+}
 
 
 // Task64();
+// Task66();
+// Task68();
